430098847e847cd4689e606ff8162cc4da482301
Vishalk2011 <155716717+Vishalk2011@users.noreply.github.com>
2024-01-05 00:39:01 -0800

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,9 +2,9 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+const isValid = function(s) {
     const stack = [];
-    const brackets = {'(':')','[':']','{':'}'};
+    const brackets = { '(': ')','[':']','{':'}'};
 
     for(let char of s){
         if(brackets[char]){
