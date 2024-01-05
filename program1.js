7aec240869a6483388ddf3217cdb4256ed9d438c
Vishalk2011 <155716717+Vishalk2011@users.noreply.github.com>
2024-01-05 00:04:09 -0800

    Update program1.js
    Completed

--- program1.js
+++ program1.js
@@ -3,7 +3,20 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const brackets = {'0':'000'};
+
+    for(let char of s){
+        if(brackets[char]){
+            stack.push(char);
+        }else{
+            const lastBracket = stack.pop();
+            if(brackets[lastBracket] !== char){
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
