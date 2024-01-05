dfdfd5eb45e61334f505b21bb95089a9dae20036
Vishalk2011 <155716717+Vishalk2011@users.noreply.github.com>
2024-01-05 00:26:23 -0800

    Update program2.js
    Updated

--- program2.js
+++ program2.js
@@ -3,7 +3,31 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    let romanNumberals(){
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 50,
+            'M' : 1000
+    };
+    let result = 0;
+    for(let i = 0; i < s.length; i++){
+        const currentNumeral = romanNumerals[s[i]];
+
+        const nextNumerals = romanNumerals[s[i+1]];
+
+        if(nextNumeral && currentNumeral < nextNumeral){
+            result += (nextNumeral - currentNumeral);
+            i++;
+        }else{
+            result += currentNumeral;
+        }
+    }
+    return result;
+
+        
 };
 
 
